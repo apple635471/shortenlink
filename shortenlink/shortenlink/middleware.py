e6f7c1a2998f1473f66b88332a1f766700717846
Shane <apple635471@gmail.com>
2024-05-09 19:38:55 +0800

    fix: wrong local only checking

--- shortenlink/shortenlink/middleware.py
+++ shortenlink/shortenlink/middleware.py
@@ -9,12 +9,13 @@ class LocalhostOnlyMiddleware:
 
     def __call__(self, request):
         # Only allow access to Django admin from Docker host
-        remote_addr = request.META.get("REMOTE_ADDR")
+        remote_addr = request.META.get(
+            "HTTP_X_REAL_IP", request.META.get("REMOTE_ADDR")
+        )
         allow_host = os.environ.get("ALLOW_ADMIN_HOST")
-        print(remote_addr)
         if allow_host:
             if request.path.startswith("/admin") and remote_addr != allow_host:
-                return HttpResponseForbidden("<h1>Forbidden</h1>")
+                return HttpResponseForbidden(f"<h1>Forbidden {remote_addr}</h1>")
 
         response = self.get_response(request)
         return response
